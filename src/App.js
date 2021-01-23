68c16f06fb5d124bcb8d3d9a4dfb2d7f185c54aa
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-01-23 19:40:48 +0500

    shabbir bhai adde

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World  AZ i'm back baby shabbir bhai 
+      Hello World  AZ i'm back baby
+      <i>shabbir bhai</i>
     </div>
   );
 }
- 
+
 export default App;
