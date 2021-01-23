febec548f3d58f5cca0ab63cac0a2ad32da95bc9
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-01-23 19:11:11 +0500

    travis sending

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,14 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/AZ/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
